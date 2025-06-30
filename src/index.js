1be18167b86c4d6c5e9f3ba83201267dd6a9655e
danielfundorc <danielfundorc@gmail.com>
2025-06-29 21:01:18 -0400

    Adding static site to server

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import ordersRoutes from "./routes/orders.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import buffetRoutes from "./routes/buffet.routes.js";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
@@ -19,10 +21,6 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("welcome");
-});
-
 app.use("/api/products", productsRoutes);
 
 app.use("/api/orders", ordersRoutes);
@@ -30,6 +28,23 @@ app.use("/api/orders", ordersRoutes);
 app.use("/api/user", userRoutes);
 
 app.use("/api/buffet", buffetRoutes);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Sirve los archivos estáticos de React
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// Para que cualquier ruta no API devuelva index.html (React Router)
+app.get("/{*any}", (req, res) => {
+  const filePath = path.join(__dirname, "../client/dist", "index.html");
+  res.sendFile(filePath, function (err) {
+    if (err) {
+      console.error("Error al enviar index.html:", err);
+      res.status(500).send("No se pudo cargar index.html");
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port: ", PORT);
